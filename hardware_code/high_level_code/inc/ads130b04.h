570db6e37e2ddd3734a7da6b2a60c33fa1242655
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-07-24 00:37:11 -0400

    update ads driver read register command, need to write tests

--- hardware_code/high_level_code/inc/ads130b04.h
+++ hardware_code/high_level_code/inc/ads130b04.h
@@ -1,16 +1,21 @@
+/**
+ * @file ads130b04.h
+ * @author sahil kale
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Note: this driver is inspired by https://github.com/LucasEtchezuri/Arduino-ADS131M04
+ * 
+ * NOTE: This driver only accomodates for 24-bit word size as it is the default.
+ * 
+ */
+
 #ifndef ADS130B04_H
 #define ADS130B04_H
 #include "adc.h" //Get ADC data types
-
-//Note: this driver is inspired by https://github.com/LucasEtchezuri/Arduino-ADS131M04
-
-/**
- * @brief Set the Channel PGA gain
- * 
- * @param channel channel to set gain for
- * @param pga programmable gain amplifier gain
- */
-void setChannelPGA(uint8_t channel, uint16_t pga);
 
 /**
  * @brief Write to single register
@@ -33,18 +38,9 @@ void writeRegisterMasked(uint8_t reg, uint8_t value, uint8_t mask);
  * @brief Read from single register
  * 
  * @param reg register address
- * @return uint16_t value read from register
+ * @return uint16_t value of register
  */
-void readRegister(uint8_t reg, uint16_t *value);
-
-/**
- * @brief Read from multiple registers
- * 
- * @param reg register address
- * @param value value to write to register
- * @param mask Mask to apply to register to only set bits that are set in mask
- */
-void readRegisterMasked(uint8_t reg, uint8_t *value, uint8_t mask);
+uint16_t readRegister(uint8_t reg);
 
 /**
  * @brief Set the Global Chop variable
@@ -62,5 +58,13 @@ void setGlobalChop(bool enable);
  * @note this driver assumes that the clock ONLY uses the high power mode. 
  */
 void setClock(bool use_external, uint8_t OSR);
+
+/**
+ * @brief Set the Channel PGA gain
+ * 
+ * @param channel channel to set gain for
+ * @param pga programmable gain amplifier gain
+ */
+void setChannelPGA(uint8_t channel, uint16_t pga);
 
 #endif /* ADS130B04_H */
--- hardware_code/high_level_code/src/ads130b04.c
+++ hardware_code/high_level_code/src/ads130b04.c
@@ -1,9 +1,16 @@
 #include "adc.h"
 #ifdef USE_ADS130B04
+//In order to avoid the compilier warning "Conversion"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+
 #include "hal_ads130b04.h"
+#include "ads130b04.h"
 #include <stdio.h>
+#include <string.h>
 
 //PRIVATE DECLARATIONS
+#define ADS130B04_SPI_WORD_SIZE_BYTES (24U)
 
 //PUBLIC DEFINITIONS
 void init_adc(void)
@@ -11,6 +18,7 @@ void init_adc(void)
     set_sync_reset_pin(true);
     for(size_t i = 0; i<2000; i++); //Crude Delay but fast enough for the ADS130B04 to reset.
     set_sync_reset_pin(false);
+
 }
 
 adc_data_t get_adc_data(void)
@@ -23,6 +31,42 @@ adc_data_t get_adc_data(void)
     return data;
 }
 
-//PRIVATE DEFINITIONS
 
+
+uint16_t readRegister(uint8_t reg)
+{
+    static const uint16_t CMD_RREG = 0xA000U; //0b1010 0000 0000 0000
+    static const uint16_t CMD_RREG_ADDR_OFFSET = 13U;
+    
+    uint8_t txBuffer[ADS130B04_SPI_WORD_SIZE_BYTES] = {0};
+    uint8_t rxBuffer[ADS130B04_SPI_WORD_SIZE_BYTES] = {0};
+
+    uint16_t command = CMD_RREG;
+
+    uint16_t addr = reg;
+    command |= (addr << CMD_RREG_ADDR_OFFSET); //append address
+    //Copy the data into txBuffer, but can't use memset as this is little endian
+    txBuffer[0] = (command >> 8) & 0xFF;
+    txBuffer[1] = command & 0xFF;
+
+    //Send the command
+    ads130b04_spi_transfer(txBuffer, sizeof(txBuffer), rxBuffer, sizeof(rxBuffer));
+    memset(txBuffer, 0, sizeof(txBuffer)); //Clear the TX buffer
+    //we now need to read the ADC data for 5 cycles (4 channels + CRC)
+    for(size_t i = 0; i < 5; i++)
+    {
+        ads130b04_spi_transfer(txBuffer, sizeof(txBuffer), rxBuffer, sizeof(rxBuffer));
+    }
+
+    //Read the response which should contain RREG response
+    ads130b04_spi_transfer(txBuffer, sizeof(txBuffer), rxBuffer, sizeof(rxBuffer));
+    uint16_t returnValue = rxBuffer[0] | (rxBuffer[1] << 8);
+    for(size_t i = 0; i < 5; i++)
+    {
+        ads130b04_spi_transfer(txBuffer, sizeof(txBuffer), rxBuffer, sizeof(rxBuffer));
+    }
+    return returnValue;
+}
+
+#pragma GCC diagnostic pop
 #endif // USE_ADS130B04
