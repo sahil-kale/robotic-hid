0175695b2eb4d7398c8c7f2abb27f2a4492f9420
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-07-24 02:08:32 -0400

    add in secondary test

--- hardware_code/high_level_code/test/test_ads130b04.cpp
+++ hardware_code/high_level_code/test/test_ads130b04.cpp
@@ -65,6 +65,48 @@ TEST(ads130b04_test, read_register_message_generation)
 
 }
 
+TEST(ads130b04_test, read_register_message_generation_clock_register)
+{
+    //Test Conditions, register = 0, return value = 0x1234
+    uint8_t register_value = 0x03U;
+    uint16_t return_value = 0xFFFFU;
+    uint8_t testTxBuffer[3] = {0};
+    testTxBuffer[0] = (0xA0U) | (register_value >> 1U);
+    testTxBuffer[1] = (register_value & 1U) << 7U;
+    uint8_t testRxBuffer[3] = {0xFF, 0xFF, 0x00};
+    uint8_t emptyBuffer[3] = {0};
+
+
+
+    mock_c()->ignoreOtherCalls();
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", testTxBuffer, sizeof(testTxBuffer))
+            ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+    
+    for(size_t i = 0; i < 5; i++)
+    {
+        mock_c()->expectOneCall("ads130b04_spi_transfer")
+                ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+                ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+    }
+
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", testRxBuffer, sizeof(testRxBuffer));
+
+    for(size_t i = 0; i < 5; i++)
+    {
+        mock_c()->expectOneCall("ads130b04_spi_transfer")
+                ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+                ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+    }
+    
+    uint16_t result = readRegister(register_value);
+
+    CHECK_EQUAL(return_value, result);
+
+}
+
 //create a test for that test group
 TEST(ads130b04_test, sync_reset_test){
     CHECK_EQUAL(0,0);
