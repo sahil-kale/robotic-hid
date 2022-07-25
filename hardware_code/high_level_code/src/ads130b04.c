b820be4ec7b7910764db6cdddad5c08be25679e8
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-07-24 22:22:33 -0400

    add in write register command

--- hardware_code/high_level_code/src/ads130b04.c
+++ hardware_code/high_level_code/src/ads130b04.c
@@ -31,6 +31,51 @@ adc_data_t get_adc_data(void)
     return data;
 }
 
+void writeRegister(uint8_t reg, uint16_t value)
+{
+    static const uint16_t CMD_WREG = 0x6000U; //0b01100000;
+    static const uint16_t CMD_WREG_ADDR_OFFSET = 7U;
+
+    uint8_t txBuffer[ADS130B04_SPI_WORD_SIZE_BYTES] = {0};
+    uint8_t rxBuffer[ADS130B04_SPI_WORD_SIZE_BYTES] = {0};
+
+    //Load the txBuffer with the command and the address
+    uint16_t command = CMD_WREG;
+    uint16_t addr = reg & 0x7FU; //0b01111111;
+    command |= (addr << CMD_WREG_ADDR_OFFSET);
+
+    //copy the command to the txBuffer
+    txBuffer[0] = (command >> 8) & 0xFF;
+    txBuffer[1] = command & 0xFF;
+
+    //Send the command
+    ads130b04_spi_transfer(txBuffer, rxBuffer, sizeof(txBuffer));
+    //clear the txBuffer
+    memset(txBuffer, 0, sizeof(txBuffer));
+    //Load the txBuffer with the value to write to the register
+    txBuffer[0] = (value >> 8) & 0xFF;
+    txBuffer[1] = value & 0xFF;
+    //Send the value
+    ads130b04_spi_transfer(txBuffer, rxBuffer, sizeof(txBuffer));
+    //clear the txBuffer
+    memset(txBuffer, 0, sizeof(txBuffer));
+
+    //Loop through 4 more calls to get to response
+    for(size_t i = 0; i < 4; i++)
+    {
+        ads130b04_spi_transfer(txBuffer, rxBuffer, sizeof(txBuffer));
+    }
+
+    //read the response
+    ads130b04_spi_transfer(txBuffer, rxBuffer, sizeof(txBuffer));
+    //uint16_t returnData = uint16_t(rxBuffer[0] << 8) | uint16_t(rxBuffer[1]);
+
+    for(size_t i = 0; i < 5; i++)
+    {
+        ads130b04_spi_transfer(txBuffer, rxBuffer, sizeof(txBuffer));
+    }
+
+}
 
 
 uint16_t readRegister(uint8_t reg)
--- hardware_code/high_level_code/test/test_ads130b04.cpp
+++ hardware_code/high_level_code/test/test_ads130b04.cpp
@@ -1,6 +1,10 @@
 #include "CppUTest/TestHarness.h"
 #include <CppUTestExt/MockSupport_c.h>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+
 extern "C" {
 #include "adc.h"
 #include "ads130b04.h"
@@ -107,8 +111,41 @@ TEST(ads130b04_test, read_register_message_generation_clock_register)
 
 }
 
+
 //create a test for that test group
-TEST(ads130b04_test, sync_reset_test){
-    CHECK_EQUAL(0,0);
+TEST(ads130b04_test, write_register_message_generation){
+    //Test conditions, register = 0x03, value = 0x1234
+    uint8_t register_value = 0x03U;
+    uint16_t value = 0x1234U;
+
+    uint8_t testTxBuffer[3] = {0};
+    testTxBuffer[0] = (0x60U) | (register_value >> 1U);
+    testTxBuffer[1] = (register_value & 1U) << 7U;
+
+    uint8_t testTxBufferWithValue[3] = {0};
+    testTxBufferWithValue[0] = value >> 8U & 0xFF;
+    testTxBufferWithValue[1] = value & 0xFF;
+
+    uint8_t emptyBuffer[3] = {0};
+
+    //This driver doesn't care about return data. Assert that it calls the SPI driver with the correct data.
+    mock_c()->ignoreOtherCalls();
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", testTxBuffer, sizeof(testTxBuffer))
+            ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", testTxBufferWithValue, sizeof(testTxBufferWithValue))
+            ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+
+    for(size_t i = 0; i < 10; i++)
+    {
+        mock_c()->expectOneCall("ads130b04_spi_transfer")
+                ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+                ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+    }
+
+    writeRegister(register_value, value);
 }
 
+#pragma GCC diagnostic pop
