c046cef72dc43e7f58fa9191c8a9c330aa41f324
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-07-26 00:47:54 -0400

    add in unit tests for ads130b04

--- hardware_code/high_level_code/test/test_ads130b04.cpp
+++ hardware_code/high_level_code/test/test_ads130b04.cpp
@@ -149,4 +149,58 @@ TEST(ads130b04_test, write_register_message_generation){
     writeRegister(register_value, value);
 }
 
+TEST(ads130b04_test, get_adc_data)
+{
+    mock_c()->ignoreOtherCalls();
+    uint8_t emptyBuffer[3] = {0};
+
+    //Have 4 separate receive buffers for each of the 4 channels.
+    //buffer contents: 0x7FFF, 0x8000, 0x0001, 0x0040
+    uint8_t testRxBuffer1[3] = {0x7F, 0xFF, 0x00}; //decimal value is 32767
+    uint8_t testRxBuffer2[3] = {0x80, 0x00, 0x00}; //decimal value is -32768
+    uint8_t testRxBuffer3[3] = {0x00, 0x01, 0x00}; //decimal value is 1
+    uint8_t testRxBuffer4[3] = {0x40, 0x00, 0x00}; //decimal value is -1
+    adc_data_t expected_data = {0};
+    expected_data.adc_data[0] = 32767;
+    expected_data.adc_data[1] = -32768;
+    expected_data.adc_data[2] = 1;
+    expected_data.adc_data[3] = 0x0040;
+
+    //Transfer null code in response
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+
+    //Transfer the first buffer ADC data
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", testRxBuffer1, sizeof(testRxBuffer1));
+    
+    //Transfer the second buffer data
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", testRxBuffer2, sizeof(testRxBuffer2));
+    
+    //Transfer the third buffer data
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", testRxBuffer3, sizeof(testRxBuffer3));
+
+    //Transfer the fourth buffer data
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", testRxBuffer4, sizeof(testRxBuffer4));
+    
+    //Transfer CRC, in this case empty
+    mock_c()->expectOneCall("ads130b04_spi_transfer")
+            ->withMemoryBufferParameter("txBuffer", emptyBuffer, sizeof(emptyBuffer))
+            ->withOutputParameterReturning("rxBuffer", emptyBuffer, sizeof(emptyBuffer));
+    
+    adc_data_t received_data = get_adc_data();
+    MEMCMP_EQUAL(&expected_data.adc_data[0], &received_data.adc_data[0], 2);
+    MEMCMP_EQUAL(&expected_data.adc_data[1], &received_data.adc_data[1], 2);
+    MEMCMP_EQUAL(&expected_data.adc_data[2], &received_data.adc_data[2], 2);
+    MEMCMP_EQUAL(&expected_data.adc_data[3], &received_data.adc_data[3], 2);
+}
+
 #pragma GCC diagnostic pop
