68dc1fd08a06d8706033d76866535e306a238838
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-07-31 01:35:38 -0400

    add in lcd header file

---
+++ hardware_code/high_level_code/inc/lcd.h
@@ -0,0 +1,40 @@
+#ifndef LCD_H
+#define LCD_H
+#include <stdint.h>
+
+#define USE_NHDC0220
+#define LCD_NUM_ROWS (2U)
+#define LCD_NUM_COLS (20U)
+
+void init_lcd(void);
+
+/**
+ * @brief Write a string to the LCD
+ * 
+ * @param string The string to write to the LCD
+ * @param length The length of the string to write to the LCD
+ * @param line The line to write the string to
+ */
+void write_lcd(char *string, size_t length, uint8_t line);
+
+/**
+ * @brief Clear the LCD screen
+ * 
+ */
+void clear_lcd(void);
+
+/**
+ * @brief Set the lcd backlight brightness
+ * 
+ * @param brightness 
+ */
+void set_lcd_backlight(uint8_t brightness);
+
+/**
+ * @brief Set the lcd contrast level
+ * 
+ * @param contrast 
+ */
+void set_lcd_contrast(uint8_t contrast);
+
+#endif //LCD_H
