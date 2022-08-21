c70556e9e0fedc9274d6162bfdbf02b05b6cc22c
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-08-20 22:56:15 -0400

    LCD sprintf works

--- hardware_code/Core/Src/application.c
+++ hardware_code/Core/Src/application.c
@@ -4,14 +4,17 @@
 #include "hal_application.h"
 #include "cmsis_os2.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 static void send_joystick_report(const gameHID_t *report);
+static void buttonlogic(void);
 
 //Static Game HId inststnace for LCD to update
 static gameHID_t hid_data = {0};
 
-//static bool left_button_down = false;
-//static bool right_button_down = false;
+static bool left_button_down = false;
+static bool right_button_down = false;
+static bool enter_button_down = false;
 
 //Create HID report task loop:
 void joystick_task(void const * argument)
@@ -29,13 +32,34 @@ void joystick_task(void const * argument)
     hid_data.JoyRX = (int8_t)(adc_data.adc_data[2]/256);
     hid_data.JoyRY = (int8_t)(adc_data.adc_data[3]/256);
 
-    //Sample Buttons:
-    hal_application_button_state_t button_state = update_button_states();
+    buttonlogic();
 
-	hid_data.Buttons = button_state.manipulating_button_state & 0b00001111;
 	send_joystick_report(&hid_data);
     osDelay(5); //200Hz
     }
+}
+
+static void buttonlogic(void)
+{
+    //Sample Buttons:
+    hal_application_button_state_t button_state = update_button_states();
+    //Set left and right button requests
+    if(button_state.lcd_button_left)
+    {
+        left_button_down = true;
+    }
+
+    if(button_state.lcd_button_right)
+    {
+        right_button_down = true;
+    }
+
+    if(button_state.lcd_button_enter)
+    {
+        enter_button_down = true;
+    }
+
+	hid_data.Buttons = button_state.manipulating_button_state & 0b00001111;
 }
 
 //Create LCD update task loop
@@ -45,9 +69,19 @@ void lcd_task(void const * argument)
     set_lcd_cursor(0,0);
     char testArray[] = "Starting...";
     write_lcd(testArray, sizeof(testArray));
+    
+    static char line_one_buffer[20] = {0};
+    static char line_two_buffer[20] = {0};
+    
     while(1)
     {
-
+        //Update LCD with joystick data from HID report using sprintf to populate buffers
+        sprintf(line_one_buffer, "LX/LY: %d/%d", hid_data.JoyLX, hid_data.JoyLY);
+        sprintf(line_two_buffer, "RX/RY: %d/%d", hid_data.JoyRX, hid_data.JoyRY);
+        set_lcd_cursor(0,0);
+        write_lcd(line_one_buffer, sizeof(line_one_buffer));
+        set_lcd_cursor(1,0);
+        write_lcd(line_two_buffer, sizeof(line_two_buffer));
         osDelay(100); //10Hz
     }
 }
