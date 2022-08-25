1c2d68552291876372058ce0e788cce24cac6e66
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-08-25 14:29:25 -0500

    create dfu ack tests

--- high_level_code/test/test_dfu.cpp
+++ high_level_code/test/test_dfu.cpp
@@ -1,15 +1,32 @@
 #include "CppUTest/TestHarness.h"
-#include "dfu.h"
+#include <CppUTestExt/MockSupport_c.h>
 
+extern "C" {
+#include "dfu.h"
+#include "hal_dfu.h"
+}
 
 // create a test group
 TEST_GROUP(dfu_tests){
+   void setup()
+    {
+        mock_c()->clear();
+    }
 
+    void teardown()
+    {
+        mock_c()->clear(); //if you ever get a bunch of memory leak errors, it's cause you forgot this line
+    }
    
 };
 
 //create a test for that test group
-TEST (dfu_tests, pass_me){
-    CHECK_EQUAL(1,1);
+TEST (dfu_tests, dfu_ack_check){
+    uint16_t test_data_len = 500; //in hex, 0x0150
+    uint8_t compare_buffer[] = {0x01, DFU_PACKET_DATA, 0x01, 0xF4};
+
+    mock_c()->expectOneCall("send_data_to_dfu_host")->withUnsignedLongIntParameters("size", sizeof(compare_buffer))->withMemoryBufferParameter("data", compare_buffer, sizeof(compare_buffer));
+
+    dfu_ack(test_data_len);
 }
 
