7d7d9b209ef7abd85d58c899c0417e99ee3ec6bb
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-08-25 13:32:36 -0500

    add in test cpp file

---
+++ high_level_code/test/test_dfu.cpp
@@ -0,0 +1,16 @@
+#include "CppUTest/TestHarness.h"
+#include "dfu.h"
+
+
+// create a test group
+TEST_GROUP(dfu_tests){
+
+   
+};
+
+//create a test for that test group
+TEST (dfu_tests, pass_me){
+    CHECK_EQUAL(1,1);
+    packet_dfu_t status;
+}
+
